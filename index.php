869c846d0a715ee76b79dbe268048cdaa00bd76b
rodrigopradoc <rodrigo070998.5@hotmail.com>
2022-01-29 19:25:57 -0500

    1era actualizacion

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+include 'bd/bd.php';
+
+header('Access-Control-Allow-Origin: *');
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    if(isset($_GET['id'])){
+        $query="select * from frameworks where id=".$_GET['id'];
+        $resultado=metodoGet($query);
+        echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
+    }else{
+        $query="select * from frameworks";
+        $resultado=metodoGet($query);
+        echo json_encode($resultado->fetchAll());
+    }
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='POST'){
+    unset($_POST['METHOD']);
+    $nombre=$_POST["nombre"];
+    $lanzamiento=$_POST["lanzamiento"];
+    $desarrollador=$_POST["desarrollador"];
+    $query="insert into frameworks(nombre, lanzamiento, desarrollador) values ('$nombre','$lanzamiento','$desarrollador')";
+    $queryAutoIncrement="select MAX(id) as id from frameworks";
+    $resultado=metodoPost($query, $queryAutoIncrement);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='PUT'){
+    unset($_POST['METHOD']);
+    $id=$_GET['id'];
+    $nombre=$_POST["nombre"];
+    $lanzamiento=$_POST["lanzamiento"];
+    $desarrollador=$_POST["desarrollador"];
+    $query="UPDATE frameworks SET nombre='$nombre', lanzamiento='$lanzamiento', desarrollador='$desarrollador' WHERE id='$id'";
+    $queryAutoIncrement="select MAX(id) as id from frameworks";
+    $resultado=metodoPut($query);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='DELETE'){
+    unset($_POST['METHOD']);
+    $id=$_GET['id'];
+    $query="DELETE FROM frameworks WHERE id='$id'";
+    $queryAutoIncrement="select MAX(id) as id from frameworks";
+    $resultado=metodoDelete($query);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+header("HTTP/1.1 400 Bad Request");
+
+?>
